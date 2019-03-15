24963f95ccf027dbe1952aca91af368f41c24d0d
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2019-03-15 08:59:23 +0100

    Handle source dir detection in core build script

--- CI/build-core.sh
+++ CI/build-core.sh
@@ -2,11 +2,13 @@
 
 set -x -e
 
+SRC=$(cd "$(dirname "$0")"; cd .. ; pwd -P)
+
 # Build everything on master
 echo "==> Building INDI Core"
 mkdir -p build/libindi
 pushd build/libindi
-cmake -DINDI_BUILD_UNITTESTS=ON -DCMAKE_INSTALL_PREFIX=/usr/local/ . ../../libindi/ -DFIX_WARNINGS=ON -DCMAKE_BUILD_TYPE=$1
+cmake -DINDI_BUILD_UNITTESTS=ON -DCMAKE_INSTALL_PREFIX=/usr/local/ . ${SRC}/libindi/ -DFIX_WARNINGS=ON -DCMAKE_BUILD_TYPE=$1
 make
 sudo make install
 popd
